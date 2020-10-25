890e1f6e41fffb07c57aeef67502c66933d3cead
luolun <515235972@qq.com>
2020-10-25 16:32:11 +0800

    Initial

---
+++ src/main/java/org/luolun/ConcurrentLinkedQueueDemo/ConcurrentLinkedQueue.java
@@ -0,0 +1,170 @@
+package org.luolun.ConcurrentLinkedQueueDemo;
+
+import sun.misc.Unsafe;
+
+import java.lang.reflect.Field;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Queue;
+import java.util.function.Predicate;
+
+/**
+ * 不支持 {@link #remove(Object)}、{@link #removeIf(Predicate)}、{@link #iterator()} 操作的话，就不需要考虑节点为空的情况，
+ * 可减少edge case，实现起来比较简单，方便理解和学习；这种实现使得null也可以作为节点内容
+ * @param <E> 节点值类型
+ */
+public class ConcurrentLinkedQueue<E> implements Queue<E> {
+    public static Unsafe createUnsafe() {
+        try {
+            Class<?> unsafeClass = Class.forName("sun.misc.Unsafe");
+            Field field = unsafeClass.getDeclaredField("theUnsafe");
+            field.setAccessible(true);
+            Unsafe unsafe = (Unsafe) field.get(null);
+            return unsafe;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private static Unsafe unsafe = createUnsafe();
+
+    private static class Node<E> {
+        volatile E value;
+        volatile Node<E> next;
+
+        Node(E value) {
+            this.value = value;
+        }
+    }
+
+    private volatile Node<E> head = new Node<>(null);
+    private volatile Node<E> tail = head;
+
+    private static final long headOffset;
+    private static final long tailOffset;
+    private static final long nodeValueOffset;
+    private static final long nodeNextOffset;
+
+    static {
+        try {
+            headOffset = unsafe.objectFieldOffset(ConcurrentLinkedQueue.class.getDeclaredField("head"));
+            tailOffset = unsafe.objectFieldOffset(ConcurrentLinkedQueue.class.getDeclaredField("tail"));
+            nodeValueOffset = unsafe.objectFieldOffset(Node.class.getDeclaredField("value"));
+            nodeNextOffset = unsafe.objectFieldOffset(Node.class.getDeclaredField("next"));
+        } catch (NoSuchFieldException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private boolean casHead(Node<E> oldHead, Node<E> newHead) {
+        return unsafe.compareAndSwapObject(this, headOffset, head, newHead);
+    }
+
+    private boolean casTail(Node<E> oldTail, Node<E> newTail) {
+        return unsafe.compareAndSwapObject(this, headOffset, tail, newTail);
+    }
+
+    private boolean casValue(Node<E> node, E oldValue, E newValue) {
+        if (node == null) throw new RuntimeException("node should not be null!");
+        return unsafe.compareAndSwapObject(node, nodeValueOffset, oldValue, newValue);
+    }
+
+    private boolean casNext(Node<E> node, Node<E> oldNext, Node<E> newNext) {
+        if (node == null) throw new RuntimeException("node should not be null!");
+        return unsafe.compareAndSwapObject(node, nodeNextOffset, oldNext, newNext);
+    }
+
+    public boolean add(E e) {
+        return offer(e);
+    }
+
+    public boolean offer(E e) {
+        Node<E> p = tail;
+        while (true) {
+            if (p.next == null) {
+                Node<E> newTail = new Node<>(e);
+                if (casNext(p, null, newTail)) return true;
+            } else if (p.next == p) { // 如果当前节点已经不在链表里，则指向自己作为标记
+                p = head;
+            } else {
+                p = p.next;
+            }
+        }
+    }
+
+    public E remove() {
+        return poll();
+    }
+
+    public E poll() {
+        while (true) {
+            Node<E> p = head.next;
+            if (p == null) return null;
+            E res = p.value;
+            if (casNext(head, p, p.next)) {
+                // head.next = head.next.next;
+                return res;
+            }
+        }
+    }
+
+    public E element() {
+        if (head.next == null) throw new NoSuchElementException();
+        return head.next.value;
+    }
+
+    public E peek() {
+        if (head.next == null) return null;
+        return head.next.value;
+    }
+
+    public int size() {
+        throw new UnsupportedOperationException("先不做");
+    }
+
+    public boolean isEmpty() {
+        throw new UnsupportedOperationException("先不做");
+    }
+
+    public boolean contains(Object o) {
+        throw new UnsupportedOperationException("先不做");
+    }
+
+    public Iterator<E> iterator() {
+        throw new UnsupportedOperationException();
+    }
+
+    public Object[] toArray() {
+        throw new UnsupportedOperationException("先不做");
+    }
+
+    public <T> T[] toArray(T[] a) {
+        throw new UnsupportedOperationException("先不做");
+    }
+
+    public boolean remove(Object o) {
+        throw new UnsupportedOperationException();
+    }
+
+    public boolean containsAll(Collection<?> c) {
+        throw new UnsupportedOperationException("先不做");
+    }
+
+    public boolean addAll(Collection<? extends E> c) {
+        throw new UnsupportedOperationException("先不做");
+    }
+
+    public boolean removeAll(Collection<?> c) {
+        throw new UnsupportedOperationException("先不做");
+    }
+
+    public boolean retainAll(Collection<?> c) {
+        throw new UnsupportedOperationException("先不做");
+    }
+
+    public void clear() {
+        throw new UnsupportedOperationException("先不做");
+    }
+}
---
+++ src/test/java/org/luolun/ConcurrentLinkedQueueDemo/BaseTest.java
@@ -0,0 +1,104 @@
+package org.luolun.ConcurrentLinkedQueueDemo;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentSkipListSet;
+
+public class BaseTest {
+    public static final int RANGE = 10000;
+    private static Logger logger = LogManager.getLogger(BaseTest.class);
+
+    Queue<Integer> queue;
+
+    public BaseTest() {}
+
+    // public BaseTest(Queue<Integer> queue) {
+    //     this.queue = queue;
+    // }
+
+    public void test() throws InterruptedException {
+        // System.out.println("start");
+
+        logger.info("创建测试数据");
+        long startGenerateData = System.currentTimeMillis();
+        Set<Integer> set = new ConcurrentSkipListSet<>();
+        for (int i = 0; i < RANGE; i++) {
+            logger.info("adding " + i);
+            queue.add(i);
+            set.add(i);
+        }
+        long endGenerateData = System.currentTimeMillis();
+        logger.info("创建测试数据完毕, 耗时: " + (endGenerateData - startGenerateData) + " 毫秒");
+
+        logger.info("Create threads");
+        List<Thread> threads = new ArrayList<>(1000);
+        long startThreads = System.currentTimeMillis();
+        for (int i = 0; i < 15; i++) {
+            Thread t = new Thread(() -> {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                logger.info(String.format("线程%s开始", Thread.currentThread().getName()));
+                long threadStart = System.currentTimeMillis();
+                Integer num = null;
+                while ((num = queue.poll()) != null) {
+                    set.remove(num);
+                }
+                logger.info(
+                        String.format("线程%s结束，耗时: %d 毫秒",
+                        Thread.currentThread().getName(),
+                        System.currentTimeMillis() - threadStart)
+                );
+            });
+            t.setName("Test thread " + i);
+            threads.add(t);
+        }
+
+        for (Thread thread : threads) {
+            thread.start();
+        }
+
+
+        logger.info("等待所有线程结束...");
+        for (Thread thread : threads) {
+            thread.join();
+        }
+        long endStartThreads = System.currentTimeMillis();
+        logger.info("所有线程结束，耗时: " + (endStartThreads - startThreads) + " millis");
+
+        Object[] setArray = set.toArray();
+        Arrays.sort(setArray);
+        Assert.assertTrue("set不为空: " + Arrays.deepToString(setArray),
+                set.isEmpty());
+    }
+
+    public static class DemoTest extends BaseTest {
+        public DemoTest() {
+            this.queue = new ConcurrentLinkedQueue<>();
+        }
+
+        @Test
+        @Override
+        public void test() throws InterruptedException {
+            super.test();
+        }
+    }
+
+    public static class JDKTest extends BaseTest {
+        public JDKTest() {
+            this.queue = new java.util.concurrent.ConcurrentLinkedQueue<>();
+        }
+
+        @Test
+        @Override
+        public void test() throws InterruptedException {
+            super.test();
+        }
+    }
+}
